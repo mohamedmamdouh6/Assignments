4f9a4ba90cd0bcbd5c4cbc779ef92ed86d654d94
Mohamed Mamdouh <mohamed.mamdouh7459@gmail.com>
2025-01-31 09:30:25 +0200

    Update gitignore file

---
+++ 5-OOP/BigInt/BigInt/BigInt.h
@@ -0,0 +1,17 @@
+#ifndef BIGINT_H
+#define BIGINT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BigInt {
+private:
+	string internalNumber;
+public:
+	// Constructors
+	BigInt() {};
+	BigInt(const string& s) {};
+};
+
+#endif
