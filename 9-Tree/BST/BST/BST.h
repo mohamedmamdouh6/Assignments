4a2885fab0d0ccd749fc09e32de417505a8ab171
Mohamed Mamdouh <mohamed.mamdouh7459@gmail.com>
2025-03-18 22:11:44 +0200

    BST Added

---
+++ 9-Tree/BST/BST/BST.h
@@ -0,0 +1,115 @@
+#pragma once
+#include <iostream>
+#include <exception>
+using namespace std;
+template <class T>
+struct Node {
+	T value;
+	Node<T>* left = nullptr;
+	Node<T>* right = nullptr;
+	Node(T element) : value(element)
+	{
+		left = right = nullptr;
+	}
+};
+template <class T>
+class BST {
+private:
+	Node<T>* root = nullptr;
+	int size = 0;
+	void addRInternal(Node<T>* temp, T element)
+	{
+		if (temp->value == element)
+			throw exception("Element Already Exists!");
+		else if (temp->value > element)
+		{
+			if (!temp->left)
+				temp->left = new Node<T>(element);
+			else
+				addRInternal(temp->left, element);
+		}
+		else
+		{
+			if (!temp->right)
+				temp->right = new Node<T>(element);
+			else
+				addRInternal(temp->right, element);
+		}
+		
+	}
+	bool isExistRInternal(Node<T>* temp, T element)
+	{
+		if (!temp)
+			return false;
+		else if (temp->value == element)
+			return true;
+		else if (temp->value > element)
+			isExistRInternal(temp->left, element);
+		else
+			isExistRInternal(temp->right, element);
+	}
+public:
+	void addI(T element)
+	{
+		if (!root)
+		{
+			root = new Node<T>(element);
+		}
+		else
+		{
+			Node<T>* temp = root;
+			while (temp)
+			{
+				if (temp->value == element)
+					throw exception("Element Already Exists!");
+				else if (temp->value > element)
+				{
+					if (!temp->left)
+					{
+						temp->left = new Node<T>(element);
+						break;
+					}
+					else
+						temp = temp->left;
+				}
+				else
+				{
+					if (!temp->right)
+					{
+						temp->right = new Node<T>(element);
+						break;
+					}
+					else
+						temp = temp->right;
+				}
+			}
+		}
+		size++;
+	}
+	bool isExistI(T element)
+	{
+		Node<T>* temp = root;
+		while (temp)
+		{
+			if (temp->value == element)
+				return true;
+			else if (temp->value > element)
+				temp = temp->left;
+			else
+				temp = temp->right;
+		}
+		return false;
+	}
+	void addR(T element)
+	{
+		if (!root)
+			root = new Node<T>(element);
+		else
+			addRInternal(root, element);
+		size++;
+	}
+	bool isExistR(T element)
+	{
+		return isExistRInternal(root, element);
+	}
+};
---
+++ 9-Tree/BST/BST/main.cpp
@@ -0,0 +1,50 @@
+#include "BST.h"
+#include <iostream>
+using namespace std;
+void main()
+{
+	// Tree 1
+	BST<int> a;
+	a.addI(50);
+	a.addI(48);
+	a.addI(70);
+	a.addI(30);
+	a.addI(65);
+	a.addI(90);
+	a.addI(20);
+	a.addI(32);
+	a.addI(67);
+	a.addI(98);
+	a.addI(15);
+	a.addI(25);
+	a.addI(31);
+	a.addI(35);
+	a.addI(66);
+	a.addI(69);
+	a.addI(94);
+	a.addI(99);
+	cout << a.isExistI(99) << endl;
+	cout << a.isExistI(101) << endl;
+	// Tree 2
+	BST<int> b;
+	b.addR(50);
+	b.addR(48);
+	b.addR(70);
+	b.addR(30);
+	b.addR(65);
+	b.addR(90);
+	b.addR(20);
+	b.addR(32);
+	b.addR(67);
+	b.addR(98);
+	b.addR(15);
+	b.addR(25);
+	b.addR(31);
+	b.addR(35);
+	b.addR(66);
+	b.addR(69);
+	b.addR(94);
+	b.addR(99);
+	cout << b.isExistR(99) << endl;
+	cout << b.isExistR(101) << endl;
+}
